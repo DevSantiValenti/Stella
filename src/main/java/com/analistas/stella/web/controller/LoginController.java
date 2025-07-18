3e2f8ccea6b119a92a76554bef3ba8f6ddca8a19
SadSatan <santiagovalentixd@gmail.com>
2025-07-17 23:27:50 -0300

    Se creo el index y el login, faltan pulir muchas cosas, y hacer otras neuvas

--- src/main/java/com/analistas/stella/web/controller/LoginController.java
+++ src/main/java/com/analistas/stella/web/controller/LoginController.java
@@ -6,15 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class HomeController {
-
-    @GetMapping("/")
-    public String home(Model model) {
-
-        // model.addAttribute("titulo", "Hola cabeza de poronga");
-
-        return "index";
+public class LoginController {
+    @GetMapping("/login")
+    public String login(Model model) {
+        
+        return "login";
     }
     
-
 }
