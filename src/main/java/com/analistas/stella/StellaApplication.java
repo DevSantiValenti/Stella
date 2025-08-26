e1d82ce827d7a6ceebe63517ff931e599794befc
SadSatan <santiagovalentixd@gmail.com>
2025-08-26 15:23:37 -0300

    De vuelta a como estaba (no funciono)

--- src/main/java/com/analistas/stella/StellaApplication.java
+++ src/main/java/com/analistas/stella/StellaApplication.java
@@ -3,10 +3,6 @@ package com.analistas.stella;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class StellaApplication {
@@ -15,20 +11,6 @@ public class StellaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StellaApplication.class, args);
-	}
-
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
 	}
 
 }
