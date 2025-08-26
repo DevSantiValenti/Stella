727ab28fe05b69d6545254f07702b99ecb6c5e3b
SadSatan <santiagovalentixd@gmail.com>
2025-08-26 14:54:55 -0300

    Cambios para deployar y poder ocupar online

--- src/main/java/com/analistas/stella/StellaApplication.java
+++ src/main/java/com/analistas/stella/StellaApplication.java
@@ -3,6 +3,10 @@ package com.analistas.stella;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class StellaApplication {
@@ -11,6 +15,20 @@ public class StellaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StellaApplication.class, args);
+	}
+
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 
 }
